de18bff9a8915bcf69ccbbc733baafcc6625937a
David Tacoronte <d.tacoronte89@gmail.com>
2024-01-15 16:36:56 -0500

    Exercise 4 done

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ class Counter {
    * @param {number} initialNumber - the initial state of the count
    */
   constructor(initialNumber) {
-    // ✨ initialize whatever properties are needed
+    this.count = initialNumber
   }
 
   /**
@@ -71,7 +71,7 @@ class Counter {
    * counter.countDown() // returns 0
    */
   countDown() {
-    // ✨ implement
+    return this.count > 0 ? this.count-- : 0
   }
 }
 
